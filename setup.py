6d98dc672a2df3767fb07d24fb880646f920c792
Tremendous1192 <tremendous.duck@gmail.com>
2024-03-15 22:19:19 +0900

    自作スクリプトの作成

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'py-scripts-tremendous1192',
+    version = "0.0.1",
+    description = "My scripts",
+    long_description = "",
+    author = 'sampleman',
+    license = 'MIT',
+    classifiers=[
+        "Development Status :: 1 - Planning"
+    ]
+)
