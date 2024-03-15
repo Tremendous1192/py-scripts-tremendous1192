d6ebf9a5c1ca351676b3d37aa86f31ef0a9f5e5f
Tremendous1192 <tremendous.duck@gmail.com>
2024-03-16 07:06:49 +0900

    依存関係の修正(polars >= 0.2)

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'py-scripts-tremendous1192',
-    version = "0.0.2",
+    version = "0.0.3",
     description = "My scripts",
     long_description = "",
-    author = 'sampleman',
+    author = 'Tremendous1192',
     license = 'MIT',
     classifiers=[
         "Development Status :: 1 - Planning"
+    ],
+    install_requires=[
+        'polars >=0.2',
     ]
 )
