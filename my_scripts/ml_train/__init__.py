92f07dec8fd5b346b894225f3b378ee28e089203
Tremendous1192 <tremendous.duck@gmail.com>
2024-03-16 20:18:07 +0900

    importの修正

--- my_scripts/ml_train/__init__.py
+++ my_scripts/ml_train/__init__.py
@@ -1,1 +1,3 @@
+from my_scripts.ml_train.logistic_regression import *
+from my_scripts.ml_train.random_forest_classifier import *
 from my_scripts.ml_train.theil_sen_regressor_shuffle_split import *
