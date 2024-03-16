9ae201fd1a0193270a994551720f93b8d593c412
Tremendous1192 <tremendous.duck@gmail.com>
2024-03-16 18:46:34 +0900

    訓練データとテストデータをpolarsデータセットに変換する

--- my_scripts/polars_addon/pandas.py
+++ my_scripts/polars_addon/pandas.py
@@ -1,4 +1,15 @@
+import pandas as pd
 import polars as pl
+
+def from_pandas_to_polars(X: pd.core.frame.DataFrame,
+                          y: pd.core.series.Series,
+                          X_test: pd.core.frame.DataFrame):
+    '''
+    訓練データとテストデータをpolarsデータセットに変換する
+    '''
+    train = pl.from_pandas(data = pd.concat([X, y], axis = 1), include_index = True)
+    test = pl.from_pandas(data = X_test, include_index = True)
+    return train, test
 
 def to_pandas_datasets(df_train: pl.dataframe.frame.DataFrame,
                        df_test: pl.dataframe.frame.DataFrame,
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'py-scripts-tremendous1192',
-    version = "0.0.19",
+    version = "0.0.20",
     description = "My scripts",
     long_description = "",
     author = 'Tremendous1192',
