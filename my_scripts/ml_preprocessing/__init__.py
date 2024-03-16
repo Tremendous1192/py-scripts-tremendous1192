70962827265d986bc69d30ed7e9d73bf949deb7d
Tremendous1192 <tremendous.duck@gmail.com>
2024-03-16 17:20:58 +0900

    IsolationForestによる異常値スコア列を追加する関数

---
+++ my_scripts/ml_preprocessing/__init__.py
@@ -0,0 +1,2 @@
+from my_scripts.ml_preprocessing.isolation_forest import *
+from my_scripts.ml_preprocessing.sgd_one_class_svm import *
--- my_scripts/ml_preprocessing/isolation_forest.py
+++ my_scripts/ml_preprocessing/isolation_forest.py
@@ -1,15 +1,15 @@
 import pandas as pd
-from sklearn.linear_model import SGDOneClassSVM
+from sklearn.linear_model import IsolationForest
 
 def score_samples(X: pd.core.frame.DataFrame,
                   y: pd.core.series.Series,
                   X_test: pd.core.frame.DataFrame,
                   normal_value):
     '''
-    One Class SVMによる異常値スコア列を追加する関数
+    IsolationForestによる異常値スコア列を追加する関数
     '''
     # 学習
-    clf = SGDOneClassSVM(random_state = 1192)
+    clf = IsolationForest(random_state = 1192)
     clf.fit(X[y == normal_value])
 
     # 異常値スコアの追加
@@ -17,4 +17,4 @@ def score_samples(X: pd.core.frame.DataFrame,
     score_test = clf.score_samples(X_test)
 
     # return X, X_test
-    return X.assign(score_OneClassSVM = score_train), X_test.assign(score_OneClassSVM = score_test)
+    return X.assign(score_IsolationForest = score_train), X_test.assign(score_IsolationForest = score_test)
