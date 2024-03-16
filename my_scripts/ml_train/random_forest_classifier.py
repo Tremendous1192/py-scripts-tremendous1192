7bf6f6f4d6ac53ac59eb74c617d54594ffd7f3c5
Tremendous1192 <tremendous.duck@gmail.com>
2024-03-17 08:11:14 +0900

    ランダムフォレスト分類器の学習関数の追加とコメントの修正

--- my_scripts/ml_train/random_forest_classifier.py
+++ my_scripts/ml_train/random_forest_classifier.py
@@ -8,7 +8,7 @@ def train_roc_auc(X: pd.core.frame.DataFrame,
           n_splits: int,
           test_size: float):
     '''
-    StratifiedShuffleSplitを用いたLogisticRegression推定器の学習\
+    StratifiedShuffleSplitを用いたRandomForestClasiferの学習\
     (roc_auc_score固定)
     '''
     # 交差分割
@@ -16,7 +16,6 @@ def train_roc_auc(X: pd.core.frame.DataFrame,
 
     # 初期化
     tsr = RandomForestClassifier(class_weight = "balanced", random_state = 1192)
-    scores = []
     best_score = -1000
     best_index = None
 
@@ -26,7 +25,7 @@ def train_roc_auc(X: pd.core.frame.DataFrame,
         y_proba = tsr.predict_proba(X.iloc[valid_index, :])[:, 1]
         score = roc_auc_score(y.iloc[valid_index], y_proba)
         print(f"Fold {i}: {score}")
-        scores.append(score)
+        # 最良スコアのインデックスを残す
         if score > best_score:
             best_score = score
             best_index = train_index
@@ -35,3 +34,44 @@ def train_roc_auc(X: pd.core.frame.DataFrame,
     model = tsr.fit(X.iloc[best_index, :], y.iloc[best_index])
     print(f"Best Score {best_score}")
     return model, best_index, best_score
+
+def train_roc_auc_square(X: pd.core.frame.DataFrame,
+          y: pd.core.series.Series,
+          n_splits: int,
+          test_size: float):
+    '''
+    StratifiedShuffleSplitを用いたRandomForestClasiferの学習\
+    (roc_auc_score固定)\
+    訓練データと検証データの2つの評価関数の積で評価する
+    '''
+    # 交差分割
+    sss = StratifiedShuffleSplit(n_splits =  n_splits, test_size = test_size, random_state = 1192)
+
+    # 初期化
+    tsr = RandomForestClassifier(class_weight = "balanced", random_state = 1192)
+    best_score = -1000
+    best_index = None
+
+    # k-fold cross validation
+    for i, (train_index, valid_index) in enumerate(sss.split(X, y)):
+        # 訓練データの学習
+        tsr.fit(X.iloc[train_index, :], y.iloc[train_index])
+        y_proba = tsr.predict_proba(X.iloc[valid_index, :])[:, 1]
+        score_train = roc_auc_score(y.iloc[valid_index], y_proba)
+        # 評価データの学習
+        tsr.fit(X.iloc[valid_index, :], y.iloc[valid_index])
+        y_proba = tsr.predict_proba(X.iloc[train_index, :])[:, 1]
+        score_valid = roc_auc_score(y.iloc[train_index], y_proba)
+        # 訓練データと検証データの評価関数の積で評価する
+        score = score_train * score_valid
+        print(f"Fold {i}: {score}")
+        # 最良スコアのインデックスを残す
+        if score > best_score:
+            best_score = score
+            best_index = train_index
+    
+    # 結果
+    model = tsr.fit(X.iloc[best_index, :], y.iloc[best_index])
+    print(f"Best Score {best_score}")
+    return model, best_index, best_score
+
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'py-scripts-tremendous1192',
-    version = "0.0.28",
+    version = "0.0.29",
     description = "My scripts",
     long_description = "",
     author = 'Tremendous1192',
