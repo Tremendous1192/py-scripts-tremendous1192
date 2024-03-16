2304de2157e6209fef4dd66ee890df3d17d4f90c
Tremendous1192 <tremendous.duck@gmail.com>
2024-03-16 17:43:21 +0900

    LocalOutlierFactorによる異常値スコア列を追加する関数

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'py-scripts-tremendous1192',
-    version = "0.0.14",
+    version = "0.0.15",
     description = "My scripts",
     long_description = "",
     author = 'Tremendous1192',
