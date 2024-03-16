ab6036192150bd388f1f98d465b959197facae24
Tremendous1192 <tremendous.duck@gmail.com>
2024-03-16 17:57:02 +0900

    似たような機能なので、削除する

--- my_scripts/ml_preprocessing/anomaly_detection.py
+++ my_scripts/ml_preprocessing/anomaly_detection.py
@@ -1,43 +1,5 @@
 import pandas as pd
-from sklearn.ensemble import IsolationForest
-from sklearn.neighbors import LocalOutlierFactor
 from sklearn.linear_model import SGDOneClassSVM
-
-def isolation_forest(X: pd.core.frame.DataFrame,
-                  y: pd.core.series.Series,
-                  X_test: pd.core.frame.DataFrame,
-                  normal_value):
-    '''
-    IsolationForestによる異常値スコア列を追加する関数
-    '''
-    # 学習
-    clf = IsolationForest(random_state = 1192)
-    clf.fit(X[y == normal_value])
-
-    # 異常値スコアの追加
-    score_train = clf.score_samples(X)
-    score_test = clf.score_samples(X_test)
-
-    # return X, X_test
-    return X.assign(score_IsolationForest = score_train), X_test.assign(score_IsolationForest = score_test)
-
-def local_outlier_factor(X: pd.core.frame.DataFrame,
-                  y: pd.core.series.Series,
-                  X_test: pd.core.frame.DataFrame,
-                  normal_value):
-    '''
-    LocalOutlierFactorによる異常値スコア列を追加する関数
-    '''
-    # 学習
-    clf = LocalOutlierFactor()
-    clf.fit(X[y == normal_value])
-
-    # 異常値スコアの追加
-    score_train = clf.score_samples(X)
-    score_test = clf.score_samples(X_test)
-
-    # return X, X_test
-    return X.assign(score_IsolationForest = score_train), X_test.assign(score_IsolationForest = score_test)
 
 def sgd_one_class_svm(X: pd.core.frame.DataFrame,
                   y: pd.core.series.Series,
